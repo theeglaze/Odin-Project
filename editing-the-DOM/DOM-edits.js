59f9e631e3f8ea8dfed7c22cd45c4b91e35d2db4
Raheem Glaze <theeglaze@gmail.com>
2023-07-26 18:51:21 -0500

    EDITING DOM VIA JS

---
+++ editing-the-DOM/DOM-edits.js
@@ -0,0 +1,31 @@
+const container = document.querySelector('#container');
+
+const content = document.createElement('div');
+content.classList.add('content');
+content.textContent = 'This is the glorious text-content!';
+
+container.appendChild(content);
+
+const para = document.createElement ('p');
+para.textContent = "hey I'm red!";
+para.style.color ='red';
+
+container.appendChild(para);
+
+const head3 = document.createElement('h3');
+head3.textContent = "I'm a blue h3";
+head3.style.cssText = "color: blue";
+
+container.appendChild(head3);
+
+const box = document.createElement('div');
+box.style.cssText = "border: 2px solid black; background-color: pink";
+
+const boxHead = document.createElement('h1');
+boxHead.textContent = "I'm in a div"
+const boxPara = document.createElement('p');
+boxPara.textContent = "ME TOO!";
+
+box.appendChild(boxHead)
+box.appendChild(boxPara);
+container.appendChild(box);
---
+++ editing-the-DOM/readme.txt
@@ -0,0 +1,3 @@
+<!-- In this directory I will be practicing how to edit the DOM tree with JavaScript. (R.Glaze 26/07/2023) -->
+
+<!-- The exercises were provided by the Odin Project (TOP) under the DOM manipulation and events lesson. (R.Glaze 26/07/2023)-->
